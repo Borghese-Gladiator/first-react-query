7ab554ced219b6b82a3577a13827664cfc9c356e
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-01-11 01:26:21 -0500

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,16 +6,13 @@ function Header() {
     <div className={styles.App}>
       <header className={styles.App_header}>
         <img src={logo} className={styles.App_logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className={styles.App_link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          First React Query App
         </a>
       </header>
     </div>
