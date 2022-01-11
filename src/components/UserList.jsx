5473cb84222b54d3551f13c65dc934a11fcb3043
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-01-10 23:41:48 -0500

    Remove unused import - fix warning to deploy

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useQueryClient, useQuery, useMutation } from 'react-query'
 import { getUsers, postUsers } from "../utils/api";
 
